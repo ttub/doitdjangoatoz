c7d0218c74c282c3aed83dba7f87e0936d695152
ttub <65864276+ttub@users.noreply.github.com>
2021-10-21 05:28:02 +0900

    포스트 수정 기능 추가

--- blog/tests.py
+++ blog/tests.py
@@ -219,4 +219,40 @@ class TestView(TestCase):
         self.assertEqual(last_post.title, 'Post Form 만들기')
         self.assertEqual(last_post.author.username, 'king')
 
+    def test_update_post(self):
+        update_post_url = f'/blog/update_post/{self.post_003.pk}/'
 
+        # 로긴 ㅌ
+        response = self.client.get(update_post_url)
+        self.assertNotEqual(response.status_code, 200)
+
+        # 로긴 벗 낫 작성자
+        self.assertNotEqual(self.post_003.author.username, self.user_naru)
+        self.client.login(username=self.user_naru.username, password='somepassword')
+        response = self.client.get(update_post_url)
+        self.assertEqual(response.status_code, 403)
+
+        # 작성자 접근
+        self.client.login(username=self.post_003.author.username, password='somepassword')
+        response = self.client.get(update_post_url)
+        self.assertEqual(response.status_code, 200)
+        soup = bs(response.content, 'html.parser')
+
+        self.assertEqual('Edit Post - Blog', soup.title.text)
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('Edit Post', main_area.text)
+
+        response = self.client.post(
+            update_post_url,
+            {
+                'title': '세번째 포스트 수정',
+                'content': '하이~~ 뻑',
+                'category': self.category_music.pk
+            },
+            follow=True
+        )
+        soup = bs(response.content, 'html.parser')
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('세번째 포스트 수정', main_area.text)
+        self.assertIn('하이~~ 뻑', main_area.text)
+        self.assertIn(self.category_music.name, main_area.text)
--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     # path('<int:pk>/', views.single_post_page),
     # path('', views.index),
+    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
