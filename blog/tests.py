8fe30eeb4be81da2fd0e6896a58bd05c83e26146
ttub <65864276+ttub@users.noreply.github.com>
2021-11-03 01:01:01 +0900

    포스트 수정 때 텍스트로 태그 지정

--- blog/tests.py
+++ blog/tests.py
@@ -251,12 +251,17 @@ class TestView(TestCase):
         main_area = soup.find('div', id='main-area')
         self.assertIn('Edit Post', main_area.text)
 
+        tag_str_input = main_area.find('input', id='id_tags_str')
+        self.assertTrue(tag_str_input)
+        self.assertIn('파이썬 공부; python', tag_str_input.attrs['value'])
+
         response = self.client.post(
             update_post_url,
             {
                 'title': '세번째 포스트 수정',
                 'content': '하이~~ 뻑',
-                'category': self.category_music.pk
+                'category': self.category_music.pk,
+                'tags_str': '파이썬 공부; 한글 태그; some tag;'
             },
             follow=True
         )
@@ -265,3 +270,7 @@ class TestView(TestCase):
         self.assertIn('세번째 포스트 수정', main_area.text)
         self.assertIn('하이~~ 뻑', main_area.text)
         self.assertIn(self.category_music.name, main_area.text)
+        self.assertIn('파이썬 공부', main_area.text)
+        self.assertIn('한글 태그', main_area.text)
+        self.assertIn('some tag', main_area.text)
+        self.assertNotIn('python', main_area.text)
