7296f8f68b6a2c20ab9615a7a21bc452b62d51e1
ttub <65864276+ttub@users.noreply.github.com>
2021-11-03 02:05:13 +0900

    마크다운 적용

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from markdownx.admin import MarkdownxModelAdmin
 from .models import Post, Category, Tag
 
 
@@ -10,6 +11,6 @@ class TagAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
 
 
-admin.site.register(Post)
+admin.site.register(Post, MarkdownxModelAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag, TagAdmin)
